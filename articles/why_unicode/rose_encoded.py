5b4e799e97721beec8f0ac954837f38eaa43f17f
pgollakota <pgollakota@gmail.com>
2011-10-08 15:50:25 -0400

    Fixed the missing include file

--- articles/why_unicode/rose_encoded.py
+++ articles/why_unicode/rose_encoded.py
@@ -1,4 +1,8 @@
-rose_uni = u'\u73ab'
+#!/usr/bin/env python
+#
+# Author: Praveen Gollakota <http://shutupandship.com>
+# Date: 2011-10-08
+#end_pyfile_headerrose_uni = u'\u73ab'
 encodings = ['big5', 'big5hkscs', 'cp950', 'gb2312', 'gbk',
             'gb18030', 'hz', 'iso2022_jp_2', 'utf_16',
             'utf_16_be', 'utf_16_le', 'utf_8', 'utf_8_sig']
